f426227b181655138a4cb921a1f687acacf4bd1f
wsd <wsd5656126@foxmail.com>
2020-01-14 21:29:36 +0800

    敏捷开发

--- src/main/java/PrimeGenerator2.java
+++ src/main/java/PrimeGenerator2.java
@@ -2,27 +2,25 @@
  * @author wusd
  * @date 2020/1/14 0:31
  */
-public class PrimeGenerator {
-    private static int s;
+public class PrimeGenerator2 {
     private static boolean[] f;
-    private static int[] primes;
+    private static int[] result;
 
     public static int[] generatePrimes(int maxValue) {
         if (maxValue < 2) {
             return new int[0];
         } else {
-            initializeSieve(maxValue);
+            initializeArrayOfIntegers(maxValue);
             sieve();
             loadPrimes();
-            return primes;
+            return result;
         }
     }
 
-    public static void initializeSieve(int maxValue) {
-        s = maxValue + 1;
-        f = new boolean[s];
+    public static void initializeArrayOfIntegers(int maxValue) {
+        f = new boolean[maxValue + 1];
         int i;
-        for (i = 0; i < s; i++) {
+        for (i = 0; i < f.length; i++) {
             f[i] = true;
         }
         f[0] = f[1] = false;
@@ -31,9 +29,9 @@ public class PrimeGenerator {
     public static void sieve() {
         int i;
         int j;
-        for (i = 2; i < Math.sqrt(s) + 1; i++) {
+        for (i = 2; i < Math.sqrt(f.length) + 1; i++) {
             if (f[i]) {
-                for (j = 2 * i; j < s; j += i) {
+                for (j = 2 * i; j < f.length; j += i) {
                     f[j] = false;
                 }
             }
@@ -45,12 +43,12 @@ public class PrimeGenerator {
         int i;
         int j;
         int count = 0;
-        for (i = 0; i < s; i++) {
+        for (i = 0; i < f.length; i++) {
             if (f[i]) count++;
         }
-        primes = new int[count];
-        for (i = 0, j = 0; i < s; i++) {
-            if (f[i]) primes[j++] = i;
+        result = new int[count];
+        for (i = 0, j = 0; i < f.length; i++) {
+            if (f[i]) result[j++] = i;
         }
 
     }
